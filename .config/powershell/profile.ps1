9518e16b079b146c7b86b47ec28c90cd057832ed
จิรทีปต์ ลอยมา <jirateep.loyma02@gmail.com>
2022-06-29 03:01:29 +0700

    Add: powershell-profile

---
+++ .config/powershell/profile.ps1
@@ -0,0 +1,6 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+
+
+ oh-my-posh init pwsh --config C:\Users\koonm\AppData\Local\oh-my-posh\themes/paradox.omp.json | Invoke-Expression
